c367a465ec3885a78acdc757a3d3da8541d3e210
lyds <ly6563876@gmail.com>
2025-09-30 20:58:22 +0800

    import

--- Hands-on-ML-master/第3章 k近邻算法/my_detect.py
+++ Hands-on-ML-master/第3章 k近邻算法/my_detect.py
@@ -1,5 +1,3 @@
-import matplotlib.pyplot as plt
-import numpy as np
 import os
 
 import numpy as np
@@ -8,28 +6,30 @@ base_dir = os.path.dirname(__file__)
 os.chdir(base_dir)
 
 # 读入mnist数据集
-m_x = np.loadtxt('mnist_x', delimiter=' ')
-m_y = np.loadtxt('mnist_y')
+m_x = np.loadtxt("mnist_x", delimiter=" ")
+m_y = np.loadtxt("mnist_y")
 
 # 将数据集分为训练集和测试集
 ratio = 0.8
 split = int(len(m_x) * ratio)
 # 打乱数据
-np.random.seed(0)   # 随机种子
-idx = np.random.permutation(np.arange(len(m_x)))    # 打乱的排列索引
+np.random.seed(0)  # 随机种子
+idx = np.random.permutation(np.arange(len(m_x)))  # 打乱的排列索引
 m_x = m_x[idx]  # 按照打乱的索引，重排样本
 m_y = m_y[idx]
 x_train, x_test = m_x[:split], m_x[split:]
 y_train, y_test = m_y[:split], m_y[split:]
 
+
 # n维空间欧氏距离
 def distance(a, b):
     return np.sqrt(np.sum(np.square(a - b)))
 
+
 class KNN:
     def __init__(self, k, label_num):
         self.k = k
-        self.label_num = label_num # 类别的数量
+        self.label_num = label_num  # 类别的数量
 
     def fit(self, x_train, y_train):
         # 在类中保存训练数据
@@ -39,30 +39,31 @@ class KNN:
     def get_knn_indices(self, x):
         # 获取距离目标样本点最近的K个样本点的索引
         # 计算已知样本的距离
-        dis = list(map(lambda a: distance(a, x), self.x_train)) 
+        dis = list(map(lambda a: distance(a, x), self.x_train))
         # 按距离从小到大排序，返回排序后的索引
-        knn_indices = np.argsort(dis) 
+        knn_indices = np.argsort(dis)
         # 取最近的K个
-        knn_indices = knn_indices[:self.k] 
+        knn_indices = knn_indices[: self.k]
         return knn_indices
 
     def get_label(self, x):
         # 对KNN方法的具体实现，观察K个近邻并使用np.argmax获取其中数量最多的类别
         knn_indices = self.get_knn_indices(x)
         # 类别计数
-        label_statistic = np.zeros(shape=[self.label_num]) 
+        label_statistic = np.zeros(shape=[self.label_num])
         for index in knn_indices:
             label = int(self.y_train[index])
             label_statistic[label] += 1
         # 返回数量最多的类别
-        return np.argmax(label_statistic) 
+        return np.argmax(label_statistic)
 
-    def predict(self, x_test): 
+    def predict(self, x_test):
         # 预测样本  x_test 的类别
         predicted_test_labels = np.zeros(shape=[len(x_test)], dtype=int)
         for i, x in enumerate(x_test):
             predicted_test_labels[i] = self.get_label(x)
         return predicted_test_labels
+
 
 for k in range(1, 10):
     knn = KNN(k, label_num=10)
@@ -70,4 +71,4 @@ for k in range(1, 10):
     predicted_labels = knn.predict(x_test)
 
     accuracy = np.mean(predicted_labels == y_test)
-    print(f'K的取值为 {k}, 预测准确率为 {accuracy * 100:.1f}%')
+    print(f"K的取值为 {k}, 预测准确率为 {accuracy * 100:.1f}%")
